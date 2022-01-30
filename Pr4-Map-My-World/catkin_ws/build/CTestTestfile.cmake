3d40d1c5a663973383170fe1018511582f7d79ab
gonzalooosanz <gonzalosanz538@gmail.com>
2022-01-30 14:27:05 +0100

    Project 4 Map My World
    Mapping and SLAM

---
+++ Pr4-Map-My-World/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Desktop/P4-SLAM/Prueba/catkin_ws/src
+# Build directory: /home/robond/Desktop/P4-SLAM/Prueba/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard)
